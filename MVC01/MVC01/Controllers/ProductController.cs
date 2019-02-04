67d8e4a07eaa8719405b40a37687b14c2d206fe6
rebeckalscarlsson <rebecka.ls.carlsson@gmail.com>
2019-02-04 09:44:07 +0100

    Initial commit

---
+++ MVC01/MVC01/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC01.Controllers
+{
+
+    //[Route("product")]
+    public class ProductController : Controller
+    {
+        //[HttpGet("testy")]
+        public IActionResult Testy()
+        {
+            return View();
+        }
+
+        //[HttpGet("Yrittää")]
+        public IActionResult Yrittää()
+        {
+            return View();
+        }
+    }
+}
