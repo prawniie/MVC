80364c2cd0201a3b511994a98ae10e864c0f520f
rebeckalscarlsson <rebecka.ls.carlsson@gmail.com>
2019-02-04 17:01:33 +0100

    Initial commit på todo-projekt

---
+++ MVC - ToDo/ToDo/ToDo/Controllers/TaskController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using ToDo.Models;
+
+namespace ToDo.Controllers
+{
+    public class TaskController : Controller
+    {
+        public IHostingEnvironment _env;
+
+        public TaskController(IHostingEnvironment env)
+        {
+            _env = env;
+        }
+
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            string root = _env.ContentRootPath;
+            string filename = Path.Combine(root, "Data", "tasks.txt");
+            string[] tasks = System.IO.File.ReadAllLines(filename);
+
+            foreach (var item in tasks)
+            {
+                Task task = new Task();
+                string[] taskArray = tasks.Split(",");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ MVC - ToDo/ToDo/ToDo/Models/Task.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Ranking { get; set; }
+    }
+}
