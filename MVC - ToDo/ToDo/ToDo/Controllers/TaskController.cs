f4fac0abbf31e0bbe27a9f3ae743c94f3dc11d5e
rebeckalscarlsson <rebecka.ls.carlsson@gmail.com>
2019-02-05 11:16:42 +0100

    Snyggat till lite fixat länkar, nu bara lägga till möjlighet att lägga till och uppdatera

--- MVC - ToDo/ToDo/ToDo/Controllers/TaskController.cs
+++ MVC - ToDo/ToDo/ToDo/Controllers/TaskController.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using ToDo.Models;
 using ToDo.Services;
+using ToDo.ViewModels;
 
 namespace ToDo.Controllers
 {
@@ -27,6 +30,18 @@ namespace ToDo.Controllers
         {
             Task task = _repo.GetById(id);
             return View(task);
+        }
+
+        public IActionResult Index()
+        {
+            var vm = new TaskListVm();
+            var list = new List<SelectListItem>();
+            var tasks = _repo.GetAll();
+
+            list = tasks.Select(x => new SelectListItem() { Value = x.Id.ToString(), Text = x.Name }).ToList();
+
+            vm.AllTasks = list;
+            return View(vm);
         }
     }
 }
---
+++ MVC - ToDo/ToDo/ToDo/ViewModels/TaskListVm.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ToDo.Models;
+
+namespace ToDo.ViewModels
+{
+    public class TaskListVm
+    {
+        public List<SelectListItem> AllTasks { get; set; }
+        public Task Task { get; set; }
+    }
+}
