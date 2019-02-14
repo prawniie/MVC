b0606a0b1422053b48ccda107024c553be30037a
rebeckalscarlsson <rebecka.ls.carlsson@gmail.com>
2019-02-14 16:34:58 +0100

    Trello - Initial commit

---
+++ Trello/Trello/Trello/Models/Subscriber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trello.Models
+{
+    public class Subscriber
+    {
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
